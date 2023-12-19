480ecdae97b6ff3924b52e987c5636ad8c4daee9
User <tazik1232@gmail.com>
2023-12-19 13:55:54 +0200

    added user model, AuthenticationController, registration endpoint

--- src/main/java/com/example/bookstore/service/BookService.java
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -1,7 +1,7 @@
 package com.example.bookstore.service;
 
-import com.example.bookstore.dto.BookDto;
-import com.example.bookstore.dto.CreateBookRequestDto;
+import com.example.bookstore.dto.book.BookDto;
+import com.example.bookstore.dto.book.CreateBookRequestDto;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 
---
+++ src/main/java/com/example/bookstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.bookstore.service.impl;
+
+import com.example.bookstore.dto.user.UserRegistrationRequestDto;
+import com.example.bookstore.dto.user.UserResponseDto;
+import com.example.bookstore.exception.RegistrationException;
+import com.example.bookstore.mapper.UserMapper;
+import com.example.bookstore.model.User;
+import com.example.bookstore.repository.UserRepository;
+import com.example.bookstore.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
+        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new RegistrationException(String.format(
+                    "Can't register user with email %s", requestDto.getEmail())
+            );
+        }
+        User user = userMapper.toUser(requestDto);
+        return userMapper.toDto(userRepository.save(user));
+    }
+}
