670f371ec738b3cdd291af7c39098e3f28c464b0
User <tazik1232@gmail.com>
2023-12-26 10:46:35 +0200

    added fixes

--- src/main/java/com/example/bookstore/security/SecurityConfig.java
+++ src/main/java/com/example/bookstore/security/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/auth/**", "/error", "/swagger-ui/**")
+                                .requestMatchers("/auth/**", "/v3/api-docs/**",
+                                        "/error", "/swagger-ui/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
