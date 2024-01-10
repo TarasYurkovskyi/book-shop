1bc64e71707f3bd92a853b6df18f803cdadabad6
User <tazik1232@gmail.com>
2024-01-10 09:08:40 +0200

    added tests

--- src/main/java/com/example/bookstore/model/ShoppingCart.java
+++ src/main/java/com/example/bookstore/model/ShoppingCart.java
@@ -13,10 +13,12 @@ import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
+@Accessors(chain = true)
 @Data
 @SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
